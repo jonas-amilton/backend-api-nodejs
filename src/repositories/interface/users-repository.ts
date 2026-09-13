f727413e07bfa08faaaf362e06dd8fb59d507408
jonas-amilton <jonas96@edu.unisinos.br>
2026-09-12 23:45:56 -0300

    feat: create findById method and implement on usecase

--- src/repositories/interface/users-repository.ts
+++ src/repositories/interface/users-repository.ts
@@ -4,4 +4,5 @@ import { CreateUserDTO } from '../../dtos/create-user-dto'
 export interface UsersRepository {
   findByEmail(email: string): Promise<User | null>
   create(data: CreateUserDTO): Promise<User>
+  findById(id: string): Promise<User | null>
 }
